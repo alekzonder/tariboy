da94e3a0d45e1bb107b935d9d5894121c3b5cd65
alekzonder <alekzonder@gmail.com>
2026-09-07 13:41:21 +0000

    feat(tasks): add detailed local command help

--- internal/taskcli/taskcli.go
+++ internal/taskcli/taskcli.go
@@ -40,9 +40,8 @@ func Run(ctx context.Context, argv []string, getenv func(string) string, stdout,
 	if len(args) == 1 && args[0] == "--help-json" {
 		return runHelpJSON(ctx, stdout, stderr)
 	}
-	if len(args) == 0 || (len(args) == 1 && (args[0] == "--help" || args[0] == "-h")) {
-		fmt.Fprintln(stdout, "usage: tariboy-tasks <mine|ready|show|create|update|assign|comment|ask|move|block|relate|done|work|artifacts|questions|answer|observe> ... [--json]")
-		return 0
+	if code, handled := runTextHelp(args, stdout, stderr); handled {
+		return code
 	}
 	parsed, err := parse(args)
 	if err != nil {
